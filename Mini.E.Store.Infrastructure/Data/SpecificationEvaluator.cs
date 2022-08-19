d3a4b374d0f0f1b6567f5dbff083a842e52130fc
Durga Yamini <kodamanchiliyamini@gmail.com>
2022-08-19 16:50:56 +0530

    generic repository, automapper, wwwroot folder

---
+++ Mini.E.Store.Infrastructure/Data/SpecificationEvaluator.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mini.E.Store.Core.Models;
+using Mini.E.Store.Core.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini.E.Store.Infrastructure.Data
+{
+    public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
+    {
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> query, ISpecification<TEntity> spec)
+        {
+            if(spec.Criteria != null)
+            {
+                query = query.Where(spec.Criteria);
+            }
+            query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
+            return query;
+        }
+    }
+}
